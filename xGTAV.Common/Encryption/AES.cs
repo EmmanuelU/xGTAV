ac06806b04c8c6abdcde8fe44a0861925ea70a10
Tom <tom_clay777@live.com>
2013-09-15 20:15:45 -0400

    Added key grabbing
    Added key grabbing from key.dat or from base executable

--- xGTAV.Common/Encryption/AES.cs
+++ xGTAV.Common/Encryption/AES.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Security.Cryptography;
-
+using System.IO;
+using System.Windows.Forms;
+using xGTAV.Common.Helpers;
 namespace xGTAV.Common.Encryption
 {
     public class GTAVAES
@@ -14,8 +16,10 @@ namespace xGTAV.Common.Encryption
         //GTA V key
         public static byte[] gtaKey;
 
-        public static byte[] Decrypt(byte[] data)
+        public static byte[] Decrypt(byte[] data, int numRounds)
         {
+            if (gtaKey == null)
+                GetKey();
             if (aes == null)
             {
                 aes = new RijndaelManaged();
@@ -24,8 +28,48 @@ namespace xGTAV.Common.Encryption
                 aes.Padding = PaddingMode.Zeros;
                 decryptor = aes.CreateDecryptor();
             }
-            decryptor.TransformBlock(data, 0, (data.Length / 0x10) * 0x10, data, 0);
+            decryptor.TransformBlock(data, 0, numRounds , data, 0);
             return data;
+        }
+
+        /// <summary>
+        /// Gets the rpf key from key.dat or scans for a xex base file to get it
+        /// </summary>
+        public static void GetKey()
+        {
+            MD5 md = MD5.Create();
+            if (File.Exists("key.dat"))
+                gtaKey = File.ReadAllBytes("key.dat");
+            else
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    ofd.Title = "Open GTA V base executable...";
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        MemoryStream xex = new MemoryStream(File.ReadAllBytes(ofd.FileName));
+
+                        ///Offsets TU0
+                        int[] offsets = new int[16] { 0x16F2956, 0x16E8F3E, 0x16E9A2E, 0x16DF5F6, 0x16EA4AE, 0x16EA766, 0x16EA90E, 0x16ED3A6, 
+                                                        0x16EF0D6,  0x16EDAAE, 0x16F5426, 0x16F4146, 0x1706A0E, 0x170693E, 0x1705D5E, 0x17081EE };
+                        List<byte> key = new List<byte>();
+                        for (int i = 0; i < offsets.Length; i++)
+                        {
+                            xex.Position = offsets[i];
+                            byte[] hex = xex.ReadBytes(2);
+                            key.Add(hex[0]);
+                            key.Add(hex[1]);
+                        }
+                        gtaKey = key.ToArray();
+                    }
+                }
+            }
+            byte[] mdHash = md.ComputeHash(gtaKey);
+            string strHash = BitConverter.ToString(mdHash).Replace("-", "").ToLower();
+            if (strHash != "EAD1EA1A3870557B424BC8CF73F51018")
+                throw new Exception("Invalid RPF key!");
+            else 
+                return;
         }
     }
 }
---
+++ xGTAV.FileFormats/RPF7/RPF7File.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xGTAV.FileFormats.RPF7
+{
+    class RPF7File
+    {
+    }
+}
