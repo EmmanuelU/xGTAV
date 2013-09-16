333a1b54a631f6bcfefd613aca3d5f723c52bf7a
Tom <tom_clay777@live.com>
2013-09-15 20:26:09 -0400

    Added GNU licensing
    Added GNU V3 to files

--- xGTAV.Common/Helpers/ByteBufferHelpers.cs
+++ xGTAV.Common/Helpers/ByteBufferHelpers.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +21,7 @@ namespace xGTAV.Common.Helpers
     {
         public static void Decompress(this byte[] buffer, byte[] output) { Compression.xCompress.Decompress(buffer, output); }
         public static byte[] Compress(this byte[] buffer) { return Compression.xCompress.Compress(buffer); }
+        public static byte[] Decrypt(this byte[] buffer, int numRounds) { return Encryption.GTAVAES.Decrypt(buffer, numRounds); }
         public static byte[] Swap(this byte[] buffer)
         {
             Array.Reverse(buffer);
